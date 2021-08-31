1e3d84e7b6fdc5d9fd9823735184993e823270e0
C <earthhominid@gmail.com>
2021-08-31 17:04:12 -0300

    fixed grammatical error

--- capitalizeString.js
+++ capitalizeString.js
@@ -1,6 +1,6 @@
 // capitalize(string) takes a string and returns that string with the first character capitalized.
 
-// The form of information to be represented is atomic, therefore the data defintion will be a simple atomic data represented by a String.
+// The form of information to be represented is atomic, therefore the data defintion will be simple atomic data represented by a String.
 
 // Signature: String -> String
 // Produces "a", the function produces a string.
