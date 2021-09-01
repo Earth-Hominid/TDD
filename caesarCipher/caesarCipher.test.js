17bac96810e8d904eab16715167b6b0313a5bd45
C <earthhominid@gmail.com>
2021-09-01 17:40:22 -0300

    added uppercase test

---
+++ caesarCipher/caesarCipher.test.js
@@ -0,0 +1,14 @@
+const caesarCipher = require('./caesarCipher');
+// define examples-test
+// (expect (function ("abc" with key=2001) "bcd" ) test four digit number
+// (expect (function ("XYZ" with key=1) "YZA") ;; test capitals
+// (expect) (function ("attack at midnight" with key = -10) ;; test negative numbers
+// (expect) (function ("Turn the lock 3 times!" with key 1) ;; test punctuation.
+
+test('abc with key=1 to equal bcd', () => {
+  expect(caesarCipher('abc', 1)).toBe('bcd');
+});
+
+test('XYZ with key=1 to equal YZA', () => {
+  expect(caesarCipher('XYZ')).toBe('YZA')
+})
