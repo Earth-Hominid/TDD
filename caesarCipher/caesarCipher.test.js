38bbf6dfcd932c7a21656f689b470933dfcf0c0e
C <earthhominid@gmail.com>
2021-09-01 19:02:23 -0300

    removed NaN test, added key=0 test

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -1,9 +1,10 @@
 const caesarCipher = require('./caesarCipher');
 // define examples-test
-// (expect (function ("abc" with key=2001) "bcd" ) test four digit number
-// (expect (function ("XYZ" with key=1) "YZA") ;; test capitals
-// (expect) (function ("attack at midnight" with key = -10) ;; test negative numbers
-// (expect) (function ("Turn the lock 3 times!" with key 1) ;; test punctuation.
+// (expect (function ("abc" with key=1) "bcd" ) ;; lower case test
+// (expect (function ("XYZ" with key=1) "YZA") ;; capital case test
+// (expect) (function ("attack at midnight" with key = -10) ;; negative key test
+// (expect) (function ("Turn the lock 3 times!" with key 1) ;; punctuation test
+// (expect) (function ("abc with key = 0) ;; key set to 0 test
 
 test('abc with key=1 to equal bcd', () => {
   expect(caesarCipher('abc', 1)).toBe('bcd');
@@ -13,12 +14,15 @@ test('ABC with key=1 to equal YZA', () => {
   expect(caesarCipher('XYZ',1)).toBe('YZA')
 })
 
-//test('attack at midnight with key = -10', () => {
-//  expect(caesarCipher('attack at midnight', -10)).toBe('kddkmu kd wsnxsqrd')
-//})
+test('attack at midnight with key = -10', () => {
+  expect(caesarCipher('attack at midnight', -10)).toBe('qjjqsa qj cytdywxj')
+})
 
-test('Turn the lock 3 times! with key=1 to equal', () => {
+test('Turn the lock 3 times! with key=1', () => {
   expect(caesarCipher('Turn the lock 3 times!', 1)).toBe('Uvso uif mpdl 3 ujnft!')
 })
 
+test('abc with key=0 to be abc', () => {
+  expect(caesarCipher('abc', 0)).toBe('abc')
+})
 
