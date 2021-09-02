0e309acd32630ab34f6d00d510e55081e98b75c9
C <earthhominid@gmail.com>
2021-09-02 18:12:57 -0300

    added logic for max and min

--- array/array.js
+++ array/array.js
@@ -1,4 +1,4 @@
-// Array Analysis. Write a function that takes an array of numbers and returns 
+// Array Analysis. Write a function that takes an array of numbers and returns
 // an object with the following properties: average, min, max, and length.
 
 // array -> number ;; signature
@@ -13,22 +13,37 @@
 // (... x (define minimum value) ; Number
 // (... x (define length value)) ; Number
 
-arrayObject = [1,1,1,1]
 
-const analyze = () => {
+
+const analyze = (arrayObject) => {
   // Determine total number of entries in array:
-  const arrayLength =  arrayObject.length
+  const arrayLength = arrayObject.length;
   // // Function adding two array numbers together
-  const addition = (sum, arrayObject) => sum + arrayObject
-  // Use reduce (which is a method on array object) like we would a for loop 
-  const totalSum = arrayObject.reduce(addition, 0)
+  const addition = (sum, arrayObject) => sum + arrayObject;
+  // Use reduce (which is a method on array object) like we would a for loop
+  const totalSum = arrayObject.reduce(addition, 0);
   // Calculate average
-  const average = totalSum / arrayLength
+  const average = totalSum / arrayLength;
+  // Destructure array and save first index
+  const [firstIndexofArray] = arrayObject;
+  // Function to determine the highest number between two numbers
+  const maximum = (accumulator, arrayObject) =>
+    Math.max(accumulator, arrayObject);
+  // Calculate the highest number in the array
+  const highestNumber = arrayObject.reduce(maximum, firstIndexofArray);
+  // Function to determine the lowest number between two numbers
+  const minimum = (accumulator, arrayObject) =>
+    Math.min(accumulator, arrayObject);
+  // Calculate the lowest number in the array
+  const lowestNumber = arrayObject.reduce(minimum, firstIndexofArray);
 
-  let object = { 
+  let object = {
     length: arrayLength,
-    average: average}
-  return object
-}
+    average: average,
+    min: lowestNumber,
+    max: highestNumber,
+  };
+  return object;
+};
 
 module.exports = analyze;
