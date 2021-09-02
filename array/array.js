49e6424565efd3e53d710f130cc74d56886cedc9
C <earthhominid@gmail.com>
2021-09-02 12:25:56 -0300

    array signature, purpose, template

---
+++ array/array.js
@@ -0,0 +1,16 @@
+// Array Analysis. Write a function that takes an array of numbers and returns 
+// an object with the following properties: average, min, max, and length.
+
+// array -> number ;; signature
+// consumes an array, performs calculations, returns object with results ;; purpose
+// const "functionName" () => { } ;; stub
+// (array [x,y,z])
+// Array is (make-object Number Number)
+// perform calculations on array
+// (define (fn-for-array))
+// (... (define average value) ; Number
+// (... (define maximum value) ; Number
+// (... (define minimum value) ; Number
+// (... (define length value)) ; Number
+// Template rules used:
+//  - compound: 4 fields
