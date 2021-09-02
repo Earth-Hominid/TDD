0bfc0af9074293ec8eb9fb693164f1aa2b4909dd
C <earthhominid@gmail.com>
2021-09-02 17:39:53 -0300

    added average test

--- array/array.test.js
+++ array/array.test.js
@@ -1,5 +1,8 @@
 const analyze= require('./array');
 
 test('length of array equals 4', () => {
-  expect(analyze(arrayObject)).toEqual({length: 4});
+  expect(analyze(arrayObject)).toEqual({
+    length: 4,
+    average: 1, 
+  });
 });
